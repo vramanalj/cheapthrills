75ffc9f3f0e76719a1417813216049f22bfd4ff1
venkat <vramanalj@gmail.com>
2021-04-27 15:37:48 +0530

    integrated spotify

--- src/pages/concertsBrowser/ConcertsBrowser.js
+++ src/pages/concertsBrowser/ConcertsBrowser.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import FrostedGlassBackdrop from '../../components/frostedGlass/FrostedGlassBackdrop';
 import FrostedGlassCard from '../../components/frostedGlassCard/FrostedGlassCard';
 
-import sampl from '../../assets/backdrop1.jpg';
-
-import concertsData from '../../music-events.json';
 import SpotifyPlayer from 'react-spotify-web-playback';
-
+import artistPlaceholder from '../../assets/artistPlaceholder.jpg';
 import './ConcertsBrowser.scss';
 
 export default function ConcertsBrowser() {
 
-  function myfunction(){
-    console.log('click')
+  const location = useLocation();
+  const [concertsData, setConcertsData] = useState(location.state.artistdata);
+  const [activeArtist, setActiveArtist] = useState();
+  const [token, updateToken] = useState(location.state.token);
+  function openDirections(venue){
+    let formattedVenue = venue.replace(/ /g, '+');
+    let mapsURL='https://www.google.com/maps/dir/?api=1&destination='+formattedVenue+'+Montreal+Quebec';
+    console.log(mapsURL);
+    window.open(mapsURL, '_blank', 'noopener,noreferrer');
   }
 
   return(
@@ -21,13 +26,13 @@ export default function ConcertsBrowser() {
     <h2 className="titleOtherPages">CHEAP THRILLS</h2>
     <FrostedGlassBackdrop>
       <div className="concertsList">
-        {concertsData.map((concert,i)=>{
+        {concertsData && concertsData.map((concert,i)=>{
         return (
           <FrostedGlassCard key={`event-card-`+i}>
             <div className="eventCard">
               <div className="cardInfoSection">
                 <div className="artistCover">
-                  <img src={sampl}></img>
+                  <img src={concert.artistCoverImg===false?artistPlaceholder:concert.artistCoverImg}></img>
                 </div>
                 <div className="concertInfo">
                   <span className="artistName">{concert.ARTIST}</span>
@@ -35,40 +40,47 @@ export default function ConcertsBrowser() {
                     <span className="concertTime">{concert.DATE} {concert.TIME}</span>
                     <div title="Add to Calendar" id="addeventatc" className="addeventatc calendardiv">
                       Add to Calendar
-                      <span class="start">05/10/2021 08:00 AM</span>
-                      <span class="end">05/10/2021 10:00 AM</span>
-                      <span class="timezone">America/Los_Angeles</span>
-                      <span class="title">Summary of the event</span>
-                      <span class="description">Description of the event</span>
-                      <span class="location">Location of the event</span>
+                      <span className="start">05/10/2021 08:00 AM</span>
+                      <span className="end">05/10/2021 10:00 AM</span>
+                      <span className="timezone">America/Los_Angeles</span>
+                      <span className="title">Summary of the event</span>
+                      <span className="description">Description of the event</span>
+                      <span className="location">Location of the event</span>
                     </div>
                   </div>
-                  <div>
+                  <div onClick={()=>{openDirections(concert.VENUE)}}>
                     <span className="concertTime">{concert.VENUE}</span>
-                    <button className="directions"><i className="bi bi-map" onClick={myfunction}/>Directions</button>
+                    <button className="directions"><i className="bi bi-map"/>Directions</button>
                   </div>
                 </div>
               </div>
               <div>
-              {/* <SpotifyPlayer
-                styles={{
-                  activeColor: '#fff',
-                  bgColor: 'transparent',
-                  color: '#fff',
-                  loaderColor: '#fff',
-                  sliderColor: '#1cb954',
-                  trackArtistColor: '#ccc',
-                  trackNameColor: '#fff',
-                }}
-                token="BQAvh4GXaNzxK1e0pjRMh3TSuifa1z0qBz7R-LnSJEV6_PMKe_Tba-IRXLfdww4pwcENHJ-gX1N6aay7BYsJI6PpTW2faA3MZ31M_1ZHu4ju36ao8uwOh9U7aMBMZ3vFi-N1WpBodp6zyhK925QVbm7OhF-yMx7hkjsANLG-ZF9JK0lmNzYRoTPrIjMKsI7P"
-                uris={['spotify:artist:6HQYnRM4OzToCYPpVBInuU']}
-              /> */}
+              {concert.spotifyID?<button className="playSampleBtn" onClick={()=>setActiveArtist(concert.spotifyID)}>
+                <i className="bi bi-music-player-fill"/>Play Sample</button>
+              : <div className="noSamples"><span>No Samples found!</span></div>}
               </div>
             </div>
           </FrostedGlassCard>
         )
         })
         }
+        {activeArtist && 
+        <div className="musicPlayer">
+          <SpotifyPlayer
+            styles={{
+              activeColor: '#1DB954',
+              bgColor: '#000000',
+              color: '#1DB954',
+              loaderColor: '#1DB954',
+              sliderColor: '#1cb954',
+              trackArtistColor: '#ccc',
+              trackNameColor: '#fff',
+            }}
+            autoPlay={activeArtist?true:false}
+            token={token}
+            uris={['spotify:artist:'+activeArtist]}
+          />
+        </div>}
       </div>
     </FrostedGlassBackdrop>
     </>
--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,16 +1,66 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+
 import './Landing.scss'
 import { useHistory } from "react-router-dom";
 import axios from 'axios';
 import FrostedGlassBackdrop from '../../components/frostedGlass/FrostedGlassBackdrop';
+import concertsData from '../../music-events.json';
+
+import {JSO, Popup} from 'jso-2';
 
 export default function Landing(props) {
-
+  let axiosProvider;
   const history = useHistory();
+  let authToken = useRef();
+  function createAxiosProvider(token){
+    return axios.create ({
+      baseURL : 'https://api.spotify.com/v1/',
+      headers: {'Accept': 'application/json',
+                'Authorization':'Bearer '+ token}
+    })
+  }
+
+
 
   function openConcertsPage(){
-    history.push('/concerts')
+    history.push('/concerts',{artistdata:concertsData,token:authToken.current.access_token})
   }
+
+  function logintoSpotify(){
+    let client = new JSO({
+      client_id: "1956304451ee4685a09898316d9eaffb",
+      redirect_uri: "http://localhost:3000",
+      authorization: "https://accounts.spotify.com/authorize",
+      scopes: "streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state",
+      response_type: "token"
+    });
+    client.callback();
+    client.getToken()
+    .then((token) => {
+    	console.log("I got the token: ", token);
+      authToken.current=token;
+      axiosProvider = createAxiosProvider(token.access_token);
+      fetchArtistData();
+    })
+  }
+
+  function fetchArtistData(){
+    concertsData.map((concert)=>{
+      axiosProvider.get('https://api.spotify.com/v1/search?q='+encodeURIComponent(concert.ARTIST)+'&type=artist').then((resp)=>{
+        concert['spotifyID']=resp.data.artists.items.length>0 && resp.data.artists.items[0].id;
+        concert['artistCoverImg']=resp.data.artists.items.length>0 && 
+        resp.data.artists.items[0].images.length>0 && 
+        resp.data.artists.items[0].images[0].url;
+        return concert;
+      })
+    })
+    console.log(concertsData);
+  }
+
+  useEffect(() => {
+    logintoSpotify();
+  },[]);
+
 
   return(
     <FrostedGlassBackdrop>
